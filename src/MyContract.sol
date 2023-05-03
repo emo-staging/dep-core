7f8546ea9f1f9c138cf2748b24aee56378f66d90
James Wenzel <jameswenzel@users.noreply.github.com>
2023-05-03 12:31:21 -0700

    initial

---
+++ src/MyContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {MyStruct, MySubStruct} from "./lib/MyStruct.sol";
+import {MyInterface} from "./interface/MyInterface.sol";
+
+contract MyContract is MyInterface {
+    function run(
+        MyStruct calldata _myStruct
+    ) external override returns (MyStruct memory) {
+        // do something
+    }
+}
